aa2ee4beec1688655dadad7d9f4be8de349ccee7
Hoang Long <hblong1111@gmail.com>
2020-09-14 14:24:36 +0700

    add design LoginActivity

--- DATN/app/src/main/java/com/example/datn1/activity/SplashActivity.java
+++ DATN/app/src/main/java/com/example/datn1/activity/SplashActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.datn1.R;
+import com.example.datn1.base.BaseActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setMyColorStatusBar(R.color.bg_status_bar);
     }
 }
